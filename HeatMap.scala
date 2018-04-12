46ca1eb1241d9ef4eb0f2fcde45c88085232e96f
RegulusXiang <regulusxg.aiesec@gmail.com>
2018-04-12 00:37:51 -0400

    ETL data for drawing the heat map

---
+++ HeatMap.scala
@@ -0,0 +1,15 @@
+val data0 = sc.textFile("pubgETL/pubg_death.csv")
+val victimPos = data0.map(line => Tuple3(line.split(',')(5), line.split(',')(9), line.split(',')(10)))
+
+val MIRmap = victimPos.filter(_._1.equals("MIRAMAR"))
+val ERAmap = victimPos.filter(_._1.equals("ERANGEL"))
+
+val mir = MIRmap.map(tuple => tuple._2 + " " + tuple._3)
+val era = ERAmap.map(tuple => tuple._2 + " " + tuple._3)
+
+mir.saveAsTextFile("pubgETL/mir_death.csv")
+era.saveAsTextFile("pubgETL/era_death.csv")
+
+
+
+
