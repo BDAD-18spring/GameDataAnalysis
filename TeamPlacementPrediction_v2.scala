8fec00bbc3f29cf41320ffd1c6469afdb3bbd78f
RegulusXiang <regulusxg.aiesec@gmail.com>
2018-04-25 17:33:06 -0400

    Update

--- TeamPlacementPrediction_v2.scala
+++ TeamPlacementPrediction_v2.scala
@@ -8,23 +8,16 @@ val data = spark.read.format("libsvm").load("matches/libsvm/libsvm_data.txt")
 
 // Automatically identify categorical features, and index them.
 // Set maxCategories so features with > 4 distinct values are treated as continuous.
-val featureIndexer = new VectorIndexer()
-  .setInputCol("features")
-  .setOutputCol("indexedFeatures")
-  .setMaxCategories(4)
-  .fit(data)
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
 
 // Split the data into training and test sets (30% held out for testing).
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
 // Train a RandomForest model.
-val rf = new RandomForestRegressor()
-  .setLabelCol("label")
-  .setFeaturesCol("indexedFeatures")
+val rf = new RandomForestRegressor().setLabelCol("label").setFeaturesCol("indexedFeatures")
 
 // Chain indexer and forest in a Pipeline.
-val pipeline = new Pipeline()
-  .setStages(Array(featureIndexer, rf))
+val pipeline = new Pipeline().setStages(Array(featureIndexer, rf))
 
 // Train model. This also runs the indexer.
 val model = pipeline.fit(trainingData)
@@ -36,10 +29,7 @@ val predictions = model.transform(testData)
 predictions.select("prediction", "label", "features").show(5)
 
 // Select (prediction, true label) and compute test error.
-val evaluator = new RegressionEvaluator()
-  .setLabelCol("label")
-  .setPredictionCol("prediction")
-  .setMetricName("rmse")
+val evaluator = new RegressionEvaluator().setLabelCol("label").setPredictionCol("prediction").setMetricName("rmse")
 
 val rmse = evaluator.evaluate(predictions)
 println(s"Root Mean Squared Error (RMSE) on test data = $rmse")
